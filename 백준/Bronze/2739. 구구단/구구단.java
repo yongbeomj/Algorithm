121d77ef1ebb83ac62c06126d206e4363f0c4e02
Yongbeom Jang <alex.ybjang@gmail.com>
2023-11-04 22:30:50 +0900

    [Bronze V] Title: 구구단, Time: 236 ms, Memory: 18584 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,14 +1,12 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		
-		for (int i = 1; i <= 9; i++) {
-			System.out.println(n + " * " + i + " = " + n*i);
-		}
-	}
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(N + " * " + i + " = " + (N * i));
+        }
+    }
 }
