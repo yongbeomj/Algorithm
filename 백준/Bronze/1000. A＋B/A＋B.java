ae0d1407ac045441a060a525d77295a9ee68c904
Yongbeom Jang <alex.ybjang@gmail.com>
2023-11-06 13:00:13 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17760 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
-		
 		Scanner sc = new Scanner(System.in);
 		int A = sc.nextInt();
 		int B = sc.nextInt();
