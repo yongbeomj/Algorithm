13c5704ad931647ba3fb01fbfacd6e797bdca849
Yongbeom Jang <alex.ybjang@gmail.com>
2023-12-03 17:17:10 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 124 ms, Memory: 14400 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String word = br.readLine();
+
+        bw.write(String.valueOf(word.length()));
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
