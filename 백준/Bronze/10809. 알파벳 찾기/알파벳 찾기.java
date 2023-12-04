0b10ed4eab82021844b7ac0a2f08795149fcd9a6
Yongbeom Jang <alex.ybjang@gmail.com>
2023-12-04 23:34:11 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 136 ms, Memory: 14480 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[26];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = -1;
+        }
+
+        String word = br.readLine();
+
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+
+            if (arr[ch - 'a'] == -1) {
+                arr[ch - 'a'] = i;
+            }
+        }
+
+        for (int i : arr) {
+            System.out.printf("%d ", i);
+        }
+    }
+}
