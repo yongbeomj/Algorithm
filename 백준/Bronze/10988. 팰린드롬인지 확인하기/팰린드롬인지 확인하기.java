229a9bed3899afcc7f07175045b4f546fc95c83c
Yongbeom Jang <alex.ybjang@gmail.com>
2023-12-13 11:19:25 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 120 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        int result = 1;
+
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                result = 0;
+                break;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
