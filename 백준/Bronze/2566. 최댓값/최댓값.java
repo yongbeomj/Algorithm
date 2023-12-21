c7537f2fa79987145f8c1b692f8b5c656e9cb876
Yongbeom Jang <alex.ybjang@gmail.com>
2023-12-21 13:31:12 +0900

    [Bronze III] Title: 최댓값, Time: 136 ms, Memory: 14384 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[][] arr = new int[9][9];
+
+        // 입력값 배열에 저장
+        for (int i = 0; i < 9; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 9; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int max = -1;
+        int row = 0;
+        int col = 0;
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+                    row = i + 1;
+                    col = j + 1;
+                }
+            }
+        }
+
+        System.out.println(max);
+        System.out.printf("%d %d", row, col);
+
+        br.close();
+    }
+}
