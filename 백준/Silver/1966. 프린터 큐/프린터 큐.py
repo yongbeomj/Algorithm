a8738f6fd7268ffe18e8481595cec7422f7e3d66
Yongbeom Jang <alex.ybjang@gmail.com>
2024-01-04 16:38:01 +0900

    [Silver III] Title: 프린터 큐, Time: 76 ms, Memory: 34028 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -1,12 +1,17 @@
 from collections import deque
 
-for _ in range(int(input())):
-    N, M = map(int, input().split())
+t = int(input())  # 테스트 케이스 개수
+for _ in range(t):
+    # n : 문서 개수, m : 몇 번째로 인쇄되는지
+    n, m = map(int, input().split())
+    # 문서 입력 받아서 공백을 기준으로 데크 삽입
     queue = deque(map(int, input().split()))
-    index = [0 for _ in range(N)]
-    index[M] = 1
+    index = [0 for _ in range(n)]
+    index[m] = 1
     cnt = 0
-    while 1:
+    
+    while True:
+        # 첫번째 값 중요도가 최대일 경우
         if queue[0] == max(queue):
             if index[0] == 1:
                 break
@@ -14,7 +19,10 @@ for _ in range(int(input())):
                 queue.popleft()
                 index.pop(0)
                 cnt += 1
+
+        # 첫번째 값이 중요도가 최대가 아닐 경우
         else:
-            queue.append(queue.popleft())
+            queue.append(queue.popleft()) # 첫번째 값을 가장 뒤로 재배치 (popleft)
             index.append(index.pop(0))
-    print(cnt+1)
+
+    print(cnt + 1)
