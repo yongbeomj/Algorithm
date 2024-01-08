91bf72e7303344829dbf656fbd8932ef62c375fb
Yongbeom Jang <alex.ybjang@gmail.com>
2024-01-08 15:17:20 +0900

    [Silver II] Title: 키로거, Time: 992 ms, Memory: 43044 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.py
+++ 백준/Silver/5397. 키로거/키로거.py
@@ -5,23 +5,30 @@ from collections import deque
 n = int(sys.stdin.readline())
 
 for _ in range(n):
+    # 문자열 입력
     input_str = sys.stdin.readline().rstrip()
-    left = deque()
+    # 커서를 기준으로 left, right 리스트 구분
+    left = []
     right = deque()
 
+    # 문자열 개수만큼 반복
     for ch in input_str:
+        # 입력이 "<" 이고 left 리스트에 요소가 있다면 left.pop() 후 pop 요소를 right.appendleft()
         if ch == "<":
             if left:
                 right.appendleft(left.pop())
+        # 입력이 ">" 이고 right 리스트에 요소가 있다면 right.pop() 후 pop 요소를 left.append()
         elif ch == ">":
             if right:
                 left.append(right.popleft())
+        # 입력이 "-" 이고 left 리스트에 요소가 있다면 left.pop()
         elif ch == "-":
             if left:
                 left.pop()
+        # 그 외 입력은 left 리스트에 추가
         else:
             left.append(ch)
 
+    # left와 right 리스트 순서대로 출력
     left.extend(right)
-    print(''.join(left))
-    
+    print("".join(left))
