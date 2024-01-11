739798001e8ebcd9d8684c8818a766a5c6c1cfd3
Yongbeom Jang <alex.ybjang@gmail.com>
2024-01-11 20:42:31 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,19 @@
+import sys
+
+t = int(sys.stdin.readline())
+
+for _ in range(t):
+    n = int(sys.stdin.readline())
+    dp = [0] * (n + 1)
+
+    for i in range(1, n + 1):
+        if i == 1:
+            dp[i] = 1
+        elif i == 2:
+            dp[i] = 2
+        elif i == 3:
+            dp[i] = 4
+        else:
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+
+    print(dp[n])
