1cabcc9c5420b25226b07a2c0a870582c2a9e6ec
Yongbeom Jang <alex.ybjang@gmail.com>
2024-02-23 10:26:34 +0900

    [Bronze V] Title: 구구단, Time: 136 ms, Memory: 14468 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,12 +1,17 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
 
         for (int i = 1; i <= 9; i++) {
-            System.out.println(N + " * " + i + " = " + (N * i));
+            bw.write(String.format("%d * %d = %d", n, i, n * i));
+            bw.newLine();
         }
+
+        bw.flush();
     }
 }
