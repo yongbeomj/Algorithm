2863e756f44554eeea74b20bfc5199c78adf9504
Yongbeom Jang <alex.ybjang@gmail.com>
2024-02-28 15:59:16 +0900

    [Bronze II] Title: 상수, Time: 124 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -4,23 +4,20 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         String a = st.nextToken();
         String b = st.nextToken();
 
-        StringBuffer sbA = new StringBuffer(a);
+        StringBuilder sbA = new StringBuilder(a);
+        StringBuilder sbB = new StringBuilder(b);
         int reverseA = Integer.parseInt(sbA.reverse().toString());
-
-        StringBuffer sbB = new StringBuffer(b);
         int reverseB = Integer.parseInt(sbB.reverse().toString());
 
         int max = Math.max(reverseA, reverseB);
 
-        bw.write(String.valueOf(max));
-        br.close();
-        bw.flush();
-        bw.close();
+        System.out.println(max);
+
     }
 }
