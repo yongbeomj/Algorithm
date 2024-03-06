edafa9159904cd2b79da4844f86434c9f8a81fa1
Yongbeom Jang <alex.ybjang@gmail.com>
2024-03-06 21:36:10 +0900

    [Bronze III] Title: 약수 구하기, Time: 124 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        List<Integer> divisors = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                divisors.add(i);
+            }
+        }
+
+        if (divisors.size() < k) {
+            bw.write(String.valueOf(0));
+        } else {
+            bw.write(String.valueOf(divisors.get(k - 1)));
+        }
+
+        bw.close();
+    }
+}
