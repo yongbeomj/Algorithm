1cc04ef6884e7fdb3470cadbedfa99bce5507b54
Yongbeom Jang <alex.ybjang@gmail.com>
2024-03-21 15:07:35 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 724 ms, Memory: 49920 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken()); // x축
+            arr[i][1] = Integer.parseInt(st.nextToken()); // y축
+        }
+        br.close();
+
+        Arrays.sort(arr, (o1, o2) -> {
+            return o1[0] != o2[0] ? o1[0] - o2[0] : o1[1] - o2[1];
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
+        }
+        
+        bw.write(String.valueOf(sb));
+        bw.flush();
+        bw.close();
+    }
+}
