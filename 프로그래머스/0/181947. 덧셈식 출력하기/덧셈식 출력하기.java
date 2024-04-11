3de7322a03de1b15f073b498fa1d870972f23828
Yongbeom Jang <alex.ybjang@gmail.com>
2024-04-11 13:38:13 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 140.79 ms, Memory: 65.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        br.close();
+        System.out.printf("%d + %d = %d", a, b, a + b);
+    }
+}
