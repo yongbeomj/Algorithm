f4a8dcde8872a4ff6cf75268b950bcecfd7162bf
Yongbeom Jang <alex.ybjang@gmail.com>
2024-04-19 16:51:07 +0900

    [Bronze III] Title: 공 넣기, Time: 144 ms, Memory: 15896 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.java
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -1,25 +1,35 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int[] arr = new int[n];
-        int m = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken()); // 바구니 개수
+        int m = Integer.parseInt(st.nextToken()); // 공 넣는 횟수
 
-        for (int i = 0; i < m; i++) {
-            int a = sc.nextInt(); // 바구니 번호 (시작)
-            int b = sc.nextInt(); // 바구니 번호 (끝)
-            int c = sc.nextInt(); // 바구니에 넣을 번호
+        int[] basket = new int[n];
+        for (int p = 0; p < m; p++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
 
-            for (int j = a - 1; j < b; j++) {
-                arr[j] = c;
+            for (int q = i - 1; q < j; q++) {
+                basket[q] = k;
             }
         }
 
-        for (int k : arr) {
-            System.out.print(k + " ");
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < n; i++) {
+            sb.append(basket[i] + " ");
         }
+
+        br.close();
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 }
