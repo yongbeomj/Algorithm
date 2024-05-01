b74181691bc4e3b964fb5b687a9c47d5e5c36b00
Yongbeom Jang <alex.ybjang@gmail.com>
2024-05-01 22:40:20 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.02 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        int numer = numer1 * denom2 + numer2 * denom1;
+        int denom = denom1 * denom2;
+
+        int gcd = getGcd(numer, denom);
+
+        int[] answer = new int[2];
+        answer[0] = numer / gcd;
+        answer[1] = denom / gcd;
+
+        return answer;
+    }
+
+
+    public static int getGcd(int num1, int num2) {
+        if (num1 % num2 == 0) {
+            return num2;
+        }
+
+        return getGcd(num2, num1 % num2);
+    }
+}
