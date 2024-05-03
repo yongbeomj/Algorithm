065e293e397e92dfe06a0a29817148ad4ae01ef0
Yongbeom Jang <alex.ybjang@gmail.com>
2024-05-03 10:41:48 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.96 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            if (Character.isUpperCase(my_string.charAt(i))) {
+                sb.append(Character.toLowerCase(my_string.charAt(i)));
+            } else {
+                sb.append(Character.toUpperCase(my_string.charAt(i)) );
+            }
+        }
+
+        return sb.toString();
+    }
+}
