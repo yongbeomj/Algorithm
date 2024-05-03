80dcdf46266c037d7967ade6446f39315bf3df6a
Yongbeom Jang <alex.ybjang@gmail.com>
2024-05-03 11:03:56 +0900

    [level 0] Title: n의 배수 고르기, Time: 2.86 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int n, int[] numlist) {
+        int[] answer = Arrays.stream(numlist).filter(value -> value % n == 0).toArray();
+        return answer;
+    }
+}
