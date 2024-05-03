27ef5135f868969430c5f0a5fbf54af0670e3a8c
Yongbeom Jang <alex.ybjang@gmail.com>
2024-05-03 21:25:28 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 1.92 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String str : s.split("")) {
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+
+        List<String> keyList = new ArrayList<>(map.keySet());
+        keyList.sort(String::compareTo);
+        for (String key : keyList) {
+            if (map.get(key) == 1) {
+                answer += key;
+            }
+        }
+
+        return answer;
+    }
+}
