4839da3344d788f712793b17ce093fe4f5270f47
Yongbeom Jang <alex.ybjang@gmail.com>
2024-05-04 13:38:34 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.08 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String[] strArr = my_string.split(" ");
+
+        answer = Integer.parseInt(strArr[0]);
+
+        for (int i = 1; i < strArr.length - 1; i++) {
+            if (strArr[i].equals("+")) {
+                answer += Integer.parseInt(strArr[i + 1]);
+            } else if (strArr[i].equals("-")) {
+                answer -= Integer.parseInt(strArr[i + 1]);
+            }
+        }
+
+        return answer;
+    }
+}
