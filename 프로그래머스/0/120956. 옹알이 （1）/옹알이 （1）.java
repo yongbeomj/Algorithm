aaaea65b62a1b1665c1f9a4677462cde44418e1f
Yongbeom Jang <alex.ybjang@gmail.com>
2024-05-07 20:29:39 +0900

    [level 0] Title: 옹알이 (1), Time: 0.14 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(String[] babbling) {
+        String[] str = {"aya", "ye", "woo", "ma"};
+        int answer = 0;
+
+        for (int i = 0; i < babbling.length; i++) {
+            for (int j = 0; j < str.length; j++) {
+                if (babbling[i].contains(str[j])) {
+                    babbling[i] = babbling[i].replace(str[j], " ");
+                }
+
+                if (babbling[i].trim().isEmpty()) {
+                    answer++;
+                    break;
+                }
+
+            }
+        }
+
+        return answer;
+    }
+}
