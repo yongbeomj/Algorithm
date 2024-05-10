388c0ab5e1820a902da719a3c834092f70fe3407
Yongbeom Jang <alex.ybjang@gmail.com>
2024-05-10 22:16:26 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        double x = Math.sqrt(n);
+
+        if (x % 1 == 0) {
+            answer = (long) Math.pow(x + 1, 2);
+        } else {
+            answer = -1;
+        }
+        
+        return answer;
+    }
+}
