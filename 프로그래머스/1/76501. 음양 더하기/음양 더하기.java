71bd27fcdae1ed6fabc3d0bec444b7a143f80ab4
Yongbeom Jang <alex.ybjang@gmail.com>
2024-05-11 16:18:45 +0900

    [level 1] Title: 음양 더하기, Time: 0.05 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,14 +1,11 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+
         for (int i = 0; i < absolutes.length; i++) {
-            if (!signs[i]) {
-                absolutes[i] = -absolutes[i];
-            }
+            answer += absolutes[i] * (signs[i] ? 1 : -1);
         }
 
-        int answer = Arrays.stream(absolutes).sum();
         return answer;
     }
 }
