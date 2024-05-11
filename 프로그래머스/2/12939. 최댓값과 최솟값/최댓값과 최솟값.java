b6112b71e1de9b0acdd4701f7e3327d247942665
Yongbeom Jang <alex.ybjang@gmail.com>
2024-05-11 18:38:51 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 8.44 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,12 +1,21 @@
-import java.util.Arrays;
-
 class Solution {
     public String solution(String s) {
         String[] strArr = s.split(" ");
-        int[] numArr = Arrays.stream(strArr).mapToInt(Integer::parseInt).toArray();
-        Arrays.sort(numArr);
+        int min, max;
+        min = max = Integer.parseInt(strArr[0]);
 
-        String answer = numArr[0] + " " + numArr[numArr.length - 1];
+        for (int i = 0; i < strArr.length; i++) {
+            int num = Integer.parseInt(strArr[i]);
+            if (min > num) {
+                min = num;
+            }
+            
+            if (max < num) {
+                max = num;
+            }
+        }
+
+        String answer = min + " " + max;
         return answer;
     }
 }
