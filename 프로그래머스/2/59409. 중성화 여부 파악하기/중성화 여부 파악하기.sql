2d11efddda8eee6f0eabdceaeabdfbdcae9975bb
Yongbeom Jang <alex.ybjang@gmail.com>
2024-08-27 21:40:35 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -0,0 +1,10 @@
+-- 코드를 입력하세요
+SELECT ANIMAL_ID
+     , NAME
+     , CASE WHEN SEX_UPON_INTAKE Like '%Neutered%' THEN 'O'
+            WHEN SEX_UPON_INTAKE Like '%Spayed%' THEN 'O'
+       ELSE 'X'
+       END
+       AS 중성화
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
